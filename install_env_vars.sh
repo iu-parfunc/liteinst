a2ce637d3cb4c188f26a312c79390afac33f7181
Ryan Newton <rrnewton@gmail.com>
2013-06-28 16:25:25 -0400

    modify LIBRARY_PATH in install_env_vars

--- install_env_vars.sh
+++ install_env_vars.sh
@@ -4,3 +4,4 @@ export PIN_ROOT=`pwd`/3rdparty/pintool/
 
 export CPATH=$CPATH:/u/parfunc/opt/include
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/u/parfunc/opt/lib
+export LIBRARY_PATH=$LIBRARY_PATH:/u/parfunc/opt/lib
