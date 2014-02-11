80567d59a36dd6d5cd750f74daab44f8549650cf
Ryan Newton <rrnewton@gmail.com>
2014-02-11 11:30:32 -0500

    Edit the mainStatic target to call init manually.  This avoids our weird problems with libasm being sensitive to link method.

--- zcatoggle/example/main.c
+++ zcatoggle/example/main.c
@@ -1,7 +1,10 @@
 #include "zca-toggle.h"
 
 int main () {
-
+#ifdef STATIC_MODE
+	printf("Calling init function manually, because we're in the statically-linked mode.\n");
+        initZCAService();
+#endif
 	printf("And this text is printed from the top of \"main\".\n");
 
 	printf("hello world with a notify instruction\n");
