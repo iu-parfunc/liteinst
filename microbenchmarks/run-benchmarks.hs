9e2a0b1af1005681dcfda1d0b1a06ce27cdfc386
Ryan Newton <rrnewton@gmail.com>
2014-07-29 16:29:04 -0400

    Add a benchmark harness for microbenchmarks directory.  Small tweaks in there.

---
+++ microbenchmarks/run-benchmarks.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+-- | HSBencher script to run all the benchmarks.
+module Main where
+import HSBencher
+import HSBencher.Backend.Fusion  (defaultFusionPlugin)
+import HSBencher.Backend.Dribble (defaultDribblePlugin)
+import HSBencher.Backend.Fusion  (defaultFusionPlugin) 
+
+import Data.Monoid  (mappend)
+import qualified Data.Map as M
+import System.Environment (getEnvironment)
+import System.Directory   (setCurrentDirectory, getDirectoryContents, getCurrentDirectory)
+import System.IO.Unsafe   (unsafePerformIO)
+import System.Process     
+import GHC.Conc           (getNumProcessors)
+
+--------------------------------------------------------------------------------
+
+backoffLevels :: [Integer]
+backoffLevels = [ 10^i | i <- [0..6] ]
+
+benches :: [Benchmark DefaultParamMeaning]
+benches = 
+  [ (mkBenchmark ("sigtrap/") [] (And[])) ] ++
+  [ (mkBenchmark "zcatoggle/initOverhead/" [show numFuns] 
+      (And[ compileParam (show numFuns) ]))
+  | numFuns <- [ 2^n | n <- [1..13]] ]
+
+setVariant str = (And [Set (Variant str) (CompileParam "")])
+
+main :: IO ()
+main = do
+  putStrLn "Begin Dynaprof profiling benchmarks..."
+  defaultMainModifyConfig $ \ conf ->
+    conf{ benchlist  = benches
+                       -- 1 hour timeout
+        , runTimeOut = Just 1000 -- Erk... need a separate compile timeout.
+        , plugIns   = [ SomePlugin defaultFusionPlugin,
+                        SomePlugin defaultDribblePlugin ]
+        , harvesters = customTagHarvesterDouble "INIT_TIME"
+                        `mappend` customTagHarvesterDouble "FINAL_OVERHEAD"
+                        `mappend` harvesters conf                        
+        }
+
+
+
+compileParam str = Set NoMeaning (CompileParam str)
