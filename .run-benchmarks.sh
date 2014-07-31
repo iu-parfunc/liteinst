680cde87ef672ba0052757f3c9d5e08a3065e361
Ryan Newton <rrnewton@gmail.com>
2014-07-31 16:15:03 -0400

    Debugging prints in .run-benchmarks.sh.  Problems with bigred

--- .run-benchmarks.sh
+++ .run-benchmarks.sh
@@ -2,10 +2,13 @@
 
 # Script used by Jenkins to run benchmarks.
 
+echo "Run benchmarks script starting, located at: $0"
 root=`dirname $0`
 
 export BENCHARGS=$*
 
 cd $root
+
+echo "Switched to directory: "`pwd`
 ./.jenkins_script.sh
 make bench
