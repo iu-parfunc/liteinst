4ef9dde6663f074ae9a8fb2f3a05adaa0309e363
Joel Svensson <svenssonjoel@yahoo.se>
2014-11-10 21:18:41 -0500

    changed run-benchmarks

--- run-benchmarks.hs
+++ run-benchmarks.hs
@@ -16,98 +16,25 @@ import System.Process
 import GHC.Conc           (getNumProcessors)
 
 --------------------------------------------------------------------------------
+{-
+new setup
 
-fixed_backoffLevels :: [Integer]
--- fixed_backoffLevels = [ 10^i | i <- [0..6] ]
---fixed_backoffLevels = [ 1, 10, 100 ]
-fixed_backoffLevels = [1,5,10,50,100,500,1000,5000,10000,50000,100000,500000,1000000,5000000,10000000]
+Sampling profiler: 
+ two configuration parameters
+  SAMPLE_SIZE
+  EPOCH_PERIOD
 
--- fixed_backoffLevels = [ round (10 ** i) | i <- [0, 0.5 .. 6.5] ]
+Backoff profiler
+  SAMPLE_SIZE
 
--- [2014.08.07] Going bigger:
--- fixed_backoffLevels = [ 10^7, 10^8 ]
-
-benches :: [Benchmark DefaultParamMeaning]
-benches = 
-  [ (mkBenchmark ("benchmarks/"++name++"/"++varname++"/Makefile") [] variant) 
-     { progname = Just name }
-    -- All benchmarks support the basic variants, but grep/gzip also support pebil/pin:
-    | (name,coreVariants) <- [ ("gzip-1.6",    moreVariants)
-                             , ("grep-2.18",   moreVariants)
-                             , ("h264ref-9.3", baseVariants)
-                             , ("bzip-1.0.3",  baseVariants)
-                             , ("perl-5.8.7",  baseVariants)
-                             , ("raxml",       baseVariants)
-                             ]
-    , (varname,variant) <- [ (v, setVariant v) | v <- coreVariants ] ++
-                           [ ("dynaprof", Or [ -- empty_strat, 
-                                               resampling
-                                             , fixed_backoff
-                                             , no_backoff 
-                                             ]) ]
-  ]
- where baseVariants = ["gprof", "unprofiled" ]
-       moreVariants = baseVariants ++ ["pebil", "pin", "oprofile"]
+to decide which profiler
+ PROFILER_TYPE=
+"FIXED_BACKOFF | SAMPLING"
 
 
-fixed_backoff = Or [ And [ Set (Variant ("fixed_backoff_"++show num)) 
-                                        (RuntimeEnv "DYN_STRATEGY" "FIXED_BACKOFF")
-                         , Set NoMeaning (RuntimeEnv "DYN_SAMPLE_SIZE" (show num)) 
-                         , Set NoMeaning (RuntimeEnv "DYN_DISABLE_SELFTIMED" "1")
-                         , Set NoMeaning (RuntimeEnv "DYN_OUTPUT_TYPE" "NONE")
-                         ]
-                   | num <- fixed_backoffLevels ]
-
-empty_strat = Set (Variant ("empty_strat"))
-                  (RuntimeEnv "DYN_STRATEGY" "EMPTY")
-
--- | This version varies both the backoff count and the epoch length
---   (time before global reenable of probes).
-resampling = Or [ And [ Set (Variant ("resampling_"++show num++"_"++show period)) 
-                                        (RuntimeEnv "DYN_STRATEGY" "SAMPLING")
-                      , Set NoMeaning (RuntimeEnv "DYN_SAMPLE_SIZE" (show num)) 
-                      , Set NoMeaning (RuntimeEnv "DYN_SAMPLE_PERIOD" (show period))
-                      , Set NoMeaning (RuntimeEnv "DYN_DISABLE_SELFTIMED" "1")
-                      , Set NoMeaning (RuntimeEnv "DYN_OUTPUT_TYPE" "NONE")
-                      ]
---                   | num    <- [ 10^i | i <- [0..5] ] -- Hold back a little more
-                   | num <- [10^i | i <- [3..8]] 
---                   | num    <- [ 10^6, 10^7, 10^8 ]
-
---                   , period <- [ 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0 ] 
-                   , period <- [ 0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0] ]
-
-no_backoff = Set (Variant "no_backoff") (RuntimeEnv "DYN_STRATEGY" "NO_BACKOFF")
-
-----------------------------------------
--- OLD, SCRAP:
-----------------------------------------
-
-bop_05 = And [Set (Variant "bop_simple_05_10000") (RuntimeEnv "DYN_STRATEGY" "BOP_SIMPLE")
-             ,Set NoMeaning                       (RuntimeEnv "DYN_SAMPLE_SIZE" "10000")
-             ,Set NoMeaning                       (RuntimeEnv "DYN_OVERHEAD" "0.05")]
-         
-bop_50 = And [Set (Variant "bop_simple_50_10000") (RuntimeEnv "DYN_STRATEGY" "BOP_SIMPLE")
-             ,Set NoMeaning                       (RuntimeEnv "DYN_SAMPLE_SIZE" "10000")
-             ,Set NoMeaning                       (RuntimeEnv "DYN_OVERHEAD" "0.50")] --should be 50 right ?? 
-
-count_only = Set (Variant "count_only") (RuntimeEnv "DYN_STRATEGY" "COUNT_ONLY")
-
-fixed_backoff_10000 = And [Set (Variant "fixed_backoff_10000") (RuntimeEnv "DYN_STRATEGY" "FIXED_BACKOFF")
-                          ,Set NoMeaning                       (RuntimeEnv "DYN_SAMPLE_SIZE" "10000")]
-
-fixed_backoff_1000000 = And [Set (Variant "fixed_backoff_1000000") (RuntimeEnv "DYN_STRATEGY" "FIXED_BACKOFF")
-                            ,Set NoMeaning                       (RuntimeEnv "DYN_SAMPLE_SIZE" "1000000")]
-
-
-
--- varied "dynaprof" sample_rate sample_size
---varied sr ss = And [ Set (Variant ("Varied_" ++ show sr ++ "_" ++ show ss)) (RunTimeEnv "DYN_STRATEGY" "SAMPLING")
---                   , Set NoMeaning                                          (RunTimeEnv "DYN_SAMPLE_SIZE" (show ss))
---                   , Set NoMeaning                                          (RunTimeEnv "DYN_SAMPLING_RATE" (show sr))] 
-
-
-setVariant str = (And [Set (Variant str) (CompileParam "")])
+EPOCH_PERIOD = "1,10,100,1000"
+SAMPLE_SIZE="100,1000,10000,100000"
+-} 
 
 main :: IO ()
 main = do
@@ -118,22 +45,188 @@ main = do
         , runTimeOut = Just 1000 -- Erk... need a separate compile timeout.
         , plugIns   = [ SomePlugin defaultFusionPlugin,
                         SomePlugin defaultDribblePlugin ]
-        , harvesters = customTagHarvesterDouble "INIT_OVERHEAD_ZCATOGGLE" `mappend` 
-                       customTagHarvesterDouble "INIT_OVERHEAD_DYNAPROF"  `mappend` 
-                       customTagHarvesterDouble "DYN_TEARDOWN"            `mappend` 
-                       customTagHarvesterInt    "NUM_SAMPLES"             `mappend` 
-                       customTagHarvesterInt    "NUM_PROBES"              `mappend` 
-                       customTagHarvesterInt    "NUM_ISLANDS"             `mappend` 
-                       customTagHarvesterInt    "NUM_EPOCHS"              `mappend` 
-                       customTagHarvesterInt    "MMAP_RETRIES"            `mappend` 
-                       customTagHarvesterInt    "TOTAL_THREADS"           `mappend` 
-                       customTagHarvesterInt    "CALLED_FUNCTIONS"        `mappend` 
-                       customTagHarvesterDouble "FINAL_OVERHEAD"   `mappend`
-                       harvesters conf
         }
 
 
--- -- | Check for a SELFTIMED line of output.
--- compilerHarvester :: LineHarvester
--- compilerHarvester = taggedLineHarvester "COMPILER" (\ d r -> r{???=d})
+
+epochs :: [Int] 
+epochs = [1,10,100,1000]
+
+samplesize :: [Int] 
+samplesize = [100,1000,10000,100000]
+
+
+benches :: [Benchmark DefaultParamMeaning]
+benches = 
+  [ (mkBenchmark ("benchmarks/"++name++"/"++varname++"/Makefile") [] variant) 
+     { progname = Just name }
+    -- All benchmarks support the basic variants, but grep/gzip also support pebil/pin:
+    | (name,coreVariants) <- [ --("gzip-1.6",    moreVariants)
+                           --  , ("grep-2.18",   moreVariants)
+                           --  , ("h264ref-9.3", baseVariants)
+                               ("bzip-1.0.3",  baseVariants)
+                             , ("perl-5.8.7",  baseVariants)
+                            -- , ("raxml",       baseVariants)
+                             , ("hmmer",       baseVariants)
+
+                             ]
+    -- Here are the Dynaprof benches
+    , (varname,variant) <- [ (v, setVariant v) | v <- coreVariants ] ++
+                           [ ("dynaprof", Or [ sampling 
+                                             , backoff  
+                                             ]) ]
+    ]
+  where baseVariants = ["gprof", "unprofiled" ]
+        moreVariants = baseVariants ++ ["pebil", "pin", "oprofile"]
+        setVariant str = (And [Set (Variant str) (CompileParam "")])
+
+sampling = Or [And [ Set (Variant ("Sampling_"++show s_size++"_"++show epoch)) (RuntimeEnv "PROFILER_TYPE" "SAMPLING")
+                   , Set NoMeaning (RuntimeEnv "SAMPLE_SIZE" (show s_size))
+                   , Set NoMeaning (RuntimeEnv "EPOCH_PERIOD" (show epoch))
+                   ]
+              | s_size <- samplesize, epoch <- epochs] 
+
+backoff  = Or [And [ Set (Variant ("Backoff_"++show s_size)) (RuntimeEnv "PROFILER_TYPE" "BACKOFF")
+                   , Set NoMeaning (RuntimeEnv "SAMPLE_SIZE" (show s_size))
+                   ]
+              | s_size <- samplesize] 
+
+
+
+
+
+
+
+
+
+
+
+---------------------------------------------------------------------------
+-- Old stuff
+---------------------------------------------------------------------------
+-- main :: IO ()
+-- main = do
+--   putStrLn "Begin Dynaprof profiling benchmarks..."
+--   defaultMainModifyConfig $ \ conf ->
+--     conf{ benchlist  = benches
+--                        -- 1 hour timeout
+--         , runTimeOut = Just 1000 -- Erk... need a separate compile timeout.
+--         , plugIns   = [ SomePlugin defaultFusionPlugin,
+--                         SomePlugin defaultDribblePlugin ]
+--         , harvesters = customTagHarvesterDouble "INIT_OVERHEAD_ZCATOGGLE" `mappend` 
+--                        customTagHarvesterDouble "INIT_OVERHEAD_DYNAPROF"  `mappend` 
+--                        customTagHarvesterDouble "DYN_TEARDOWN"            `mappend` 
+--                        customTagHarvesterInt    "NUM_SAMPLES"             `mappend` 
+--                        customTagHarvesterInt    "NUM_PROBES"              `mappend` 
+--                        customTagHarvesterInt    "NUM_ISLANDS"             `mappend` 
+--                        customTagHarvesterInt    "NUM_EPOCHS"              `mappend` 
+--                        customTagHarvesterInt    "MMAP_RETRIES"            `mappend` 
+--                        customTagHarvesterInt    "TOTAL_THREADS"           `mappend` 
+--                        customTagHarvesterInt    "CALLED_FUNCTIONS"        `mappend` 
+--                        customTagHarvesterDouble "FINAL_OVERHEAD"   `mappend`
+--                        harvesters conf
+--         }
+
+
+
+
+
+
+
+-- fixed_backoffLevels :: [Integer]
+-- -- fixed_backoffLevels = [ 10^i | i <- [0..6] ]
+-- --fixed_backoffLevels = [ 1, 10, 100 ]
+-- fixed_backoffLevels = [1,5,10,50,100,500,1000,5000,10000,50000,100000,500000,1000000,5000000,10000000]
+
+-- -- fixed_backoffLevels = [ round (10 ** i) | i <- [0, 0.5 .. 6.5] ]
+
+-- -- [2014.08.07] Going bigger:
+-- -- fixed_backoffLevels = [ 10^7, 10^8 ]
+
+-- benches :: [Benchmark DefaultParamMeaning]
+-- benches = 
+--   [ (mkBenchmark ("benchmarks/"++name++"/"++varname++"/Makefile") [] variant) 
+--      { progname = Just name }
+--     -- All benchmarks support the basic variants, but grep/gzip also support pebil/pin:
+--     | (name,coreVariants) <- [ ("gzip-1.6",    moreVariants)
+--                              , ("grep-2.18",   moreVariants)
+--                              , ("h264ref-9.3", baseVariants)
+--                              , ("bzip-1.0.3",  baseVariants)
+--                              , ("perl-5.8.7",  baseVariants)
+--                              , ("raxml",       baseVariants)
+--                              ]
+--     , (varname,variant) <- [ (v, setVariant v) | v <- coreVariants ] ++
+--                            [ ("dynaprof", Or [ -- empty_strat, 
+--                                                resampling
+--                                              , fixed_backoff
+--                                              , no_backoff 
+--                                              ]) ]
+--   ]
+--  where baseVariants = ["gprof", "unprofiled" ]
+--        moreVariants = baseVariants ++ ["pebil", "pin", "oprofile"]
+
+
+-- fixed_backoff = Or [ And [ Set (Variant ("fixed_backoff_"++show num)) 
+--                                         (RuntimeEnv "DYN_STRATEGY" "FIXED_BACKOFF")
+--                          , Set NoMeaning (RuntimeEnv "DYN_SAMPLE_SIZE" (show num)) 
+--                          , Set NoMeaning (RuntimeEnv "DYN_DISABLE_SELFTIMED" "1")
+--                          , Set NoMeaning (RuntimeEnv "DYN_OUTPUT_TYPE" "NONE")
+--                          ]
+--                    | num <- fixed_backoffLevels ]
+
+-- empty_strat = Set (Variant ("empty_strat"))
+--                   (RuntimeEnv "DYN_STRATEGY" "EMPTY")
+
+-- -- | This version varies both the backoff count and the epoch length
+-- --   (time before global reenable of probes).
+-- resampling = Or [ And [ Set (Variant ("resampling_"++show num++"_"++show period)) 
+--                                         (RuntimeEnv "DYN_STRATEGY" "SAMPLING")
+--                       , Set NoMeaning (RuntimeEnv "DYN_SAMPLE_SIZE" (show num)) 
+--                       , Set NoMeaning (RuntimeEnv "DYN_SAMPLE_PERIOD" (show period))
+--                       , Set NoMeaning (RuntimeEnv "DYN_DISABLE_SELFTIMED" "1")
+--                       , Set NoMeaning (RuntimeEnv "DYN_OUTPUT_TYPE" "NONE")
+--                       ]
+-- --                   | num    <- [ 10^i | i <- [0..5] ] -- Hold back a little more
+--                    | num <- [10^i | i <- [3..8]] 
+-- --                   | num    <- [ 10^6, 10^7, 10^8 ]
+
+-- --                   , period <- [ 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0 ] 
+--                    , period <- [ 0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0] ]
+
+-- no_backoff = Set (Variant "no_backoff") (RuntimeEnv "DYN_STRATEGY" "NO_BACKOFF")
+
+-- ----------------------------------------
+-- -- OLD, SCRAP:
+-- ----------------------------------------
+
+-- bop_05 = And [Set (Variant "bop_simple_05_10000") (RuntimeEnv "DYN_STRATEGY" "BOP_SIMPLE")
+--              ,Set NoMeaning                       (RuntimeEnv "DYN_SAMPLE_SIZE" "10000")
+--              ,Set NoMeaning                       (RuntimeEnv "DYN_OVERHEAD" "0.05")]
+         
+-- bop_50 = And [Set (Variant "bop_simple_50_10000") (RuntimeEnv "DYN_STRATEGY" "BOP_SIMPLE")
+--              ,Set NoMeaning                       (RuntimeEnv "DYN_SAMPLE_SIZE" "10000")
+--              ,Set NoMeaning                       (RuntimeEnv "DYN_OVERHEAD" "0.50")] --should be 50 right ?? 
+
+-- count_only = Set (Variant "count_only") (RuntimeEnv "DYN_STRATEGY" "COUNT_ONLY")
+
+-- fixed_backoff_10000 = And [Set (Variant "fixed_backoff_10000") (RuntimeEnv "DYN_STRATEGY" "FIXED_BACKOFF")
+--                           ,Set NoMeaning                       (RuntimeEnv "DYN_SAMPLE_SIZE" "10000")]
+
+-- fixed_backoff_1000000 = And [Set (Variant "fixed_backoff_1000000") (RuntimeEnv "DYN_STRATEGY" "FIXED_BACKOFF")
+--                             ,Set NoMeaning                       (RuntimeEnv "DYN_SAMPLE_SIZE" "1000000")]
+
+
+
+-- -- varied "dynaprof" sample_rate sample_size
+-- --varied sr ss = And [ Set (Variant ("Varied_" ++ show sr ++ "_" ++ show ss)) (RunTimeEnv "DYN_STRATEGY" "SAMPLING")
+-- --                   , Set NoMeaning                                          (RunTimeEnv "DYN_SAMPLE_SIZE" (show ss))
+-- --                   , Set NoMeaning                                          (RunTimeEnv "DYN_SAMPLING_RATE" (show sr))] 
+
+
+-- setVariant str = (And [Set (Variant str) (CompileParam "")])
+
+
+-- -- -- | Check for a SELFTIMED line of output.
+-- -- compilerHarvester :: LineHarvester
+-- -- compilerHarvester = taggedLineHarvester "COMPILER" (\ d r -> r{???=d})
 
