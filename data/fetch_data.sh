493c0ac1b64303c87c1e4b0ae86b9fcf6c1f3408
Joel Svensson <svenssonjoel@yahoo.se>
2014-11-10 20:27:00 -0500

    tweak script

--- data/fetch_data.sh
+++ data/fetch_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Git Depth, etc.
+
+#pass two parameters hostname then gitdepth
 GIT_DEPTH=$2
 HOSTNAME=$1
 
@@ -36,107 +37,6 @@ function fetchall
             order by PROGNAME" > all_$HOSTNAME.csv
  
 }    
-
-
-# function fetchall
-# {
-# 	echo Fetching all data.
-
-# 	# We're going to need slightly more than gitdepth to uniquely
-# 	# identify a run.
-	
-# 	# Figure 7 (static vs dynamic)
-# 	echo Fetching Figure 7 data...
-# 	echo
-# 	$Q --query="select HOSTNAME,count(),THREADS,AVERAGE(MEDIANTIME) as runtime
-#                 from FT
-#                 where GIT_DEPTH=$GIT_DEPTH
-#                   and VARIANT='cilk'
-#                   and HOSTNAME like 'd0%'
-#                   and PROGNAME='accelerate-blackscholes'
-#                 group by HOSTNAME, THREADS" > static-vs-dynamic-blackscholes.dat
-
-# 	$Q --query="select HOSTNAME,count(),THREADS,AVERAGE(MEDIANTIME) as runtime
-#                 from FT
-#                 where GIT_DEPTH=$GIT_DEPTH
-#                   and VARIANT='cilk'
-#                   and HOSTNAME like 'd0%'
-#                   and PROGNAME='accelerate-nbody-float'
-#                 group by HOSTNAME, THREADS" > static-vs-dynamic-nbody.dat
-
-# 	echo
-# 	echo
-
-# 	# Figure 9 (Black Scholes Backend Configurations)
-# 	echo Fetching Figure 9 Data...
-# 	echo
-# 	$Q --query="select HOSTNAME,count(),ARGS,AVERAGE(MEDIANTIME) as runtime
-#                 from FT
-#                 where GIT_DEPTH=$GIT_DEPTH
-#                   and VARIANT='cilk'
-#                   and HOSTNAME like 'd0%'
-#                   and PROGNAME='accelerate-blackscholes'
-#                 group by HOSTNAME, ARGS" > bs-variant-cilk.dat
-# 	$Q --query="select HOSTNAME,count(),ARGS,AVERAGE(MEDIANTIME) as runtime
-#                 from FT
-#                 where GIT_DEPTH=$GIT_DEPTH
-#                   and VARIANT='2gpu'
-#                   and HOSTNAME like 'd0%'
-#                   and PROGNAME='accelerate-blackscholes'
-#                 group by HOSTNAME, ARGS" > bs-variant-2gpu.dat
-# 	$Q --query="select HOSTNAME,count(),ARGS,AVERAGE(MEDIANTIME) as runtime
-#                 from FT
-#                 where GIT_DEPTH=$GIT_DEPTH
-#                   and VARIANT='cpugpu'
-#                   and HOSTNAME like 'd0%'
-#                   and PROGNAME='accelerate-blackscholes'
-#                 group by HOSTNAME, ARGS" > bs-variant-cpugpu.dat
-# 	$Q --query="select HOSTNAME,count(),ARGS,AVERAGE(MEDIANTIME) as runtime
-#                 from FT
-#                 where GIT_DEPTH=$GIT_DEPTH
-#                   and VARIANT='cuda'
-#                   and HOSTNAME like 'd0%'
-#                   and PROGNAME='accelerate-blackscholes'
-#                 group by HOSTNAME, ARGS" > bs-variant-cuda.dat
-
-# 	# Figure 8 (nbody and nbody++)
-# 	echo
-# 	echo
-# 	echo Fetching Figure 8 Data...
-# 	echo
-# 	$Q --query="select HOSTNAME,count(),ARGS,AVERAGE(MEDIANTIME) as runtime
-#                 from FT
-#                 where GIT_DEPTH=$GIT_DEPTH
-#                   and VARIANT='cilk'
-#                   and HOSTNAME like 'd0%'
-#                   and THREADS='12'
-#                   and PROGNAME='accelerate-nbody-float'
-#                 group by HOSTNAME, ARGS" > nbody-variant-cilk.dat
-# 	$Q --query="select HOSTNAME,count(),ARGS,AVERAGE(MEDIANTIME) as runtime
-#                 from FT
-#                 where GIT_DEPTH=$GIT_DEPTH
-#                   and VARIANT='cuda'
-#                   and HOSTNAME like 'd0%'
-#                   and PROGNAME='accelerate-nbody-float'
-#                 group by HOSTNAME, ARGS" > nbody-variant-cuda.dat
-# 	$Q --query="select HOSTNAME,count(),ARGS,AVERAGE(MEDIANTIME) as runtime
-#                 from FT
-#                 where GIT_DEPTH=$GIT_DEPTH
-#                   and VARIANT='2gpu'
-#                   and HOSTNAME like 'd0%'
-#                   and PROGNAME='accelerate-nbody-float'
-#                 group by HOSTNAME, ARGS" > nbody-variant-2gpu.dat
-# 	$Q --query="select HOSTNAME,count(),ARGS,AVERAGE(MEDIANTIME) as runtime,
-#                        AVERAGE(THREADS)
-#                 from FT
-#                 where GIT_DEPTH=$GIT_DEPTH
-#                   and VARIANT='cpugpu'
-#                   and HOSTNAME like 'd0%'
-#                   and PROGNAME='accelerate-nbody-float'
-#                 group by HOSTNAME, ARGS" > nbody-variant-cpugpu.dat
-# }
-
-
 
 
 fetchall
