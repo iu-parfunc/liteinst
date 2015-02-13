48f680b95f1cc6dd76ec108a09f3e8cef8e47c2f
chamibuddhika <chamibuddhika@gmail.com>
2015-02-13 10:13:54 -0500

    Merging branch + micro bench changes

--- microbenchmarks/ubiprof/sharedLibOverhead/app_so.cpp
+++ microbenchmarks/ubiprof/sharedLibOverhead/app_so.cpp
@@ -1,24 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include "cycle.h"
 
 void foo() {
 
 }
 
-
 int main() {
   char* n_str = getenv("N");
-  uint64_t n = 10000000;
+  // uint64_t n = 100000000;
+  uint64_t n = 316519501;
   if (n_str!= NULL) {
     n = atol(n_str);
   }
 
-  printf("N => %lu\n", n);
+  // printf("N => %lu\n", n);
 
-  for (int i=0; i < n; i++) {
+  ticks elapsed = 0;
+#pragma noinline recursive
+  for (uint64_t i=0; i < n; i++) {
+    // ticks start = getticks();
     foo();
+    // ticks end = getticks();
+    // elapsed += (end - start);
   } 
+  // ticks elapsed = end - start;
+
+  // fprintf(stderr, "Cycles per function call : %lu\n", elapsed / n);
+  
 }
 
 
--- microbenchmarks/ubiprof/sharedLibOverhead/app_static.cpp
+++ microbenchmarks/ubiprof/sharedLibOverhead/app_static.cpp
@@ -9,15 +9,18 @@ void foo() {
 
 int main() {
   char* n_str = getenv("N");
-  uint64_t n = 10000000;
+  uint64_t n = 10000000000;
   if (n_str!= NULL) {
     n = atol(n_str);
   }
 
-  printf("N => %lu\n", n);
-
-  for (int i=0; i < n; i++) {
-    foo();
+  // printf("N => %lu\n", n);
+  //
+// #pragma noinline recursive
+  uint64_t result = 0;
+  for (uint64_t i=0; i < n; i++) {
+   result++; 
+    // foo();
   } 
 }
 
