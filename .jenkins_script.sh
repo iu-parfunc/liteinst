1c1cc89aa6c3d7c8f760bc2857c7dfbc664412fc
Joel Svensson <svenssonjoel@yahoo.se>
2015-03-19 14:03:51 -0400

    HACK!

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -4,8 +4,9 @@ set -x
 set -e
 
 if [ $HOSTNAME = swarm ]; then 
- PATH=$PATH:/opt/modules/Modules/3.2.10/bin
- modulecmd bash add intel 
+ PATH=/opt/intel/parallel_studio_xe/composer_xe_2015.1.133/bin/intel64:/opt/intel/parallel_studio_xe/composer_xe_2015.1.133/bin/intel64_mic:/opt/intel/parallel_studio_xe/composer_xe_2015.1.133/debugger/gui/intel64:$PATH
+   
+ LD_LIBRARY_PATH=/opt/intel/parallel_studio_xe/composer_xe_2015.1.133/tbb/lib/intel64:/opt/intel/parallel_studio_xe/composer_xe_2015.1.133/mkl/lib/intel64:/opt/intel/parallel_studio_xe/composer_xe_2015.1.133/compiler/lib/intel64:/opt/intel/parallel_studio_xe/composer_xe_2015.1.133/ipp/lib/intel64:$LD_LIBRARY_PATH
 else 
  module add intel 
 fi 
