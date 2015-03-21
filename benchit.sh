13e9818ca05f8c76f3d1b3151d8d891d703832ef
Joel Svensson <svenssonjoel@yahoo.se>
2015-03-21 18:52:12 -0400

    update

--- benchit.sh
+++ benchit.sh
@@ -2,6 +2,6 @@
 # PERL WONT COMPILE 
 #perl-5.8.7
 for bench in hmmer lbm  sjeng bzip-1.0.3 fluid blackscholes hull nbody ; do 
-WHICHBENCH=$bench/unprofiled JENKINS_GHC=7.8.3 ./.run-benchmarks.sh $(pwd) $WHICHBENCH
-WHICHBENCH=$bench/ubiprof JENKINS_GHC=7.8.3 ./.run-benchmarks.sh $(pwd) $WHICHBENCH
+WHICHBENCH=$bench/unprofiled JENKINS_GHC=7.8.3 ./.run-benchmarks.sh $(pwd) --hostname=$HOSTNAME $WHICHBENCH
+WHICHBENCH=$bench/ubiprof JENKINS_GHC=7.8.3 ./.run-benchmarks.sh $(pwd) --hostname=$HOSTNAME $WHICHBENCH
 done
