664526e99ec7ae758c16314b4d3cac8e5bc3c481
Joel Svensson <svenssonjoel@yahoo.se>
2015-03-21 22:21:14 -0400

    update

--- benchit.sh
+++ benchit.sh
@@ -5,8 +5,8 @@
 export MACHINECLASS=$HOSTNAME
 
 # fluid blackscholes hull nbody
-
-for bench in hmmer lbm  sjeng bzip-1.0.3  ; do 
+# bzip-1.0.3
+for bench in hmmer lbm sjeng   ; do 
 WHICHBENCH=$bench/unprofiled JENKINS_GHC=7.8.3 ./.run-benchmarks.sh $(pwd)  $WHICHBENCH
 WHICHBENCH=$bench/ubiprof JENKINS_GHC=7.8.3 ./.run-benchmarks.sh $(pwd)  $WHICHBENCH
 done
