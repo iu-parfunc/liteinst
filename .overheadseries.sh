36be89c1b8ec32cc5a246980c9b8a5294881f402
Joel Svensson <svenssonjoel@yahoo.se>
2015-03-24 19:31:28 -0400

    update

--- .overheadseries.sh
+++ .overheadseries.sh
@@ -2,7 +2,7 @@
 
 
 #build ubiprof with overhead time series output capability 
-# make lib CFLAGS='-DOVERHEAD_TIME_SERIES' 
+make lib CFLAGS='-DOVERHEAD_TIME_SERIES' 
 
 
 
@@ -28,9 +28,24 @@ function run_it {
     (cd $BENCHROOT/$BENCH/ubiprof; make run) 
     # will not work for the parallel benches!! 
     # that use a different directory structure (My bad idea) 
-    cp $BENCHROOT/$BENCH/instrumented/overhead.out $DATADIR/$BENCH$ROUND.out
+    mv $BENCHROOT/$BENCH/instrumented/overhead.out $DATADIR/$HOSTNAME-$BENCH-$ROUND.out
+    mv $BENCHROOT/$BENCH/instrumented/prof.out $DATADIR/prof-$HOSTNAME-$BENCH-$ROUND.out
 }
 
+run_it_par { 
+    BENCH=$1
+    EXECDIR=$2
+    ROUND=$3
+    for retry in 1 2 3 4 5; do 
+	(cd $BENCHROOT/$BENCH/ubiprof; make run)
+	if [ $? = 0 ] ; then break 
+	else 
+	    (cd $BENCHROOT/$BENCH/$EXECDIR; rm overhead.out) 
+	fi 
+    done 
+    mv $BENCHROOT/$BENCH/$EXECDIR/overhead.out $DATADIR/$HOSTNAME-$BENCH-$ROUND.out
+    mv $BENCHROOT/$BENCH/$EXECDIR/prof.out $DATADIR/prof-$HOSTNAME-$BENCH-$ROUND.out
+}
 
 
 #ubiprof parameters 
@@ -38,7 +53,7 @@ export PROFILER_TYPE=SAMPLING
 export TARGET_OVERHEAD=5 
 
 
-for benchmark in h264ref-9.2 hmmer lbm perl-5.8.7 sjeng bzip-1.03; do 
+for benchmark in h264ref-9.3 hmmer lbm perl-5.8.7 sjeng bzip-1.03; do 
     echo "processing bench:"$benchmark
   
     build_it $benchmark ;
@@ -48,9 +63,32 @@ for benchmark in h264ref-9.2 hmmer lbm perl-5.8.7 sjeng bzip-1.03; do
 	run_it $benchmark $round 
 	
     done 
-    
-    
-
 done 
+
+#Those parallel benchmarks need special attention! 
+
+#blackscholes 
+build_it blackscholes ; 
+ for round in 1 2 3 ; do 
+	run_it blackscholes src $round 	
+ done 
+
+#fluid 
+build_it fluid ; 
+ for round in 1 2 3 ; do 
+	run_it fluid src $round 	
+ done 
+
+#hull 
+build_it hull ; 
+ for round in 1 2 3 ; do 
+	run_it hull quickHull $round 	
+ done 
+
+#nbody 
+build_it nbody ; 
+ for round in 1 2 3 ; do 
+	run_it nbody BarnesHut $round 	
+ done 
 
 
