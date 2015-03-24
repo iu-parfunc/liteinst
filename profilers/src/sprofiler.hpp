5b2cdfa87a0bb4c76bec14c85bad7120ce0dfd5c
Ryan Newton <rrnewton@gmail.com>
2015-03-24 13:08:55 -0400

    Changing default parameters to less aggresive values

--- profilers/src/sprofiler.hpp
+++ profilers/src/sprofiler.hpp
@@ -63,8 +63,8 @@ class SamplingProfiler : public Profiler, public Monitorable {
     TLStatistics** tls_stats;
     int thread_counter = 0; // Number of threads running
     uint64_t sample_size = 10000; // Size of one sample
-    uint64_t epoch_period = 10; // Monitor thread sleep period between checks in milliseconds
-    uint64_t target_overhead = 10; // Target profiling overhead cap
+    uint64_t epoch_period = 100; // Monitor thread sleep period between checks in milliseconds
+    uint64_t target_overhead = 5; // Target profiling overhead cap
 
 };
 
