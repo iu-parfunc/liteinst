9a2cf524d23f89d5245196b9f8225c1559bf17a1
Ryan Newton <rrnewton@gmail.com>
2015-03-24 19:38:09 -0400

    update

--- .overheadseries.sh
+++ .overheadseries.sh
@@ -24,18 +24,20 @@ function build_it {
 function run_it { 
     BENCH=$1
     ROUND=$2 
+    TARGOH=$3 
 
     (cd $BENCHROOT/$BENCH/ubiprof; make run) 
     # will not work for the parallel benches!! 
     # that use a different directory structure (My bad idea) 
-    mv $BENCHROOT/$BENCH/instrumented/overhead.out $DATADIR/$HOSTNAME-$BENCH-$ROUND.out
-    mv $BENCHROOT/$BENCH/instrumented/prof.out $DATADIR/prof-$HOSTNAME-$BENCH-$ROUND.out
+    mv $BENCHROOT/$BENCH/instrumented/overhead.out $DATADIR/$HOSTNAME-$BENCH-$ROUND-$TARGOH.out
+    mv $BENCHROOT/$BENCH/instrumented/prof.out $DATADIR/prof-$HOSTNAME-$BENCH-$ROUND-$TARGOH.out
 }
 
 run_it_par { 
     BENCH=$1
     EXECDIR=$2
     ROUND=$3
+    TARGOH=$4
     for retry in 1 2 3 4 5; do 
 	(cd $BENCHROOT/$BENCH/ubiprof; make run)
 	if [ $? = 0 ] ; then break 
@@ -43,52 +45,54 @@ run_it_par {
 	    (cd $BENCHROOT/$BENCH/$EXECDIR; rm overhead.out) 
 	fi 
     done 
-    mv $BENCHROOT/$BENCH/$EXECDIR/overhead.out $DATADIR/$HOSTNAME-$BENCH-$ROUND.out
-    mv $BENCHROOT/$BENCH/$EXECDIR/prof.out $DATADIR/prof-$HOSTNAME-$BENCH-$ROUND.out
+    mv $BENCHROOT/$BENCH/$EXECDIR/overhead.out $DATADIR/$HOSTNAME-$BENCH-$ROUND-$TARGOH.out
+    mv $BENCHROOT/$BENCH/$EXECDIR/prof.out $DATADIR/prof-$HOSTNAME-$BENCH-$ROUND-$TARGOH.out
 }
 
 
-#ubiprof parameters 
+#ubiprof parameters  (3 5 10) 
 export PROFILER_TYPE=SAMPLING 
-export TARGET_OVERHEAD=5 
 
+for target in 3 5 10 ; do 
 
-for benchmark in h264ref-9.3 hmmer lbm perl-5.8.7 sjeng bzip-1.03; do 
-    echo "processing bench:"$benchmark
+    export TARGET_OVERHEAD=$target 
+
+    for benchmark in h264ref-9.3 hmmer lbm perl-5.8.7 sjeng bzip-1.03; do 
+	echo "processing bench:"$benchmark
   
-    build_it $benchmark ;
+	build_it $benchmark ;
      
-    for round in 1 2 3 ; do 
+	for round in 1 2 3 ; do 
 	
-	run_it $benchmark $round 
+	    run_it $benchmark $round $target
 	
+	done 
     done 
-done 
 
 #Those parallel benchmarks need special attention! 
 
 #blackscholes 
-build_it blackscholes ; 
- for round in 1 2 3 ; do 
-	run_it blackscholes src $round 	
- done 
+    build_it blackscholes ; 
+    for round in 1 2 3 ; do 
+	run_it blackscholes src $round $target
+    done 
 
 #fluid 
-build_it fluid ; 
- for round in 1 2 3 ; do 
-	run_it fluid src $round 	
- done 
+    build_it fluid ; 
+    for round in 1 2 3 ; do 
+	run_it fluid src $round $target	
+    done 
 
 #hull 
-build_it hull ; 
- for round in 1 2 3 ; do 
-	run_it hull quickHull $round 	
- done 
+    build_it hull ; 
+    for round in 1 2 3 ; do 
+	run_it hull quickHull $round $target
+    done 
 
 #nbody 
-build_it nbody ; 
- for round in 1 2 3 ; do 
-	run_it nbody BarnesHut $round 	
- done 
+    build_it nbody ; 
+    for round in 1 2 3 ; do 
+	run_it nbody BarnesHut $round $target
+    done 
 
-
+done
