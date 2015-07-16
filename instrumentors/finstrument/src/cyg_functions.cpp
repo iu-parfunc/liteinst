88ab36b6908e1fcdf70cb7728ff5e9ab0c747fd5
Joel Svensson <svenssonjoel@yahoo.se>
2015-07-16 18:13:15 +0200

    update in cyg_functions.cpp

--- instrumentors/finstrument/src/cyg_functions.cpp
+++ instrumentors/finstrument/src/cyg_functions.cpp
@@ -377,15 +377,15 @@ inline PatchResult* patch_first_parameter(uint64_t* call_return_addr, uint64_t* 
 
   _DecodedInst inst;
 
-  _CodeInfo ci;//  = {0};
+  _CodeInfo ci  = {0};
   ci.code = (uint8_t*)start_addr;
   ci.codeLen = offset;
   ci.dt = Decode64Bits;
   ci.codeOffset = 0x100000;
-  ci.nextOffset =  { 0 };
-  ci.code = { 0 };
-  ci.codeLen = { 0 };
-  ci.features = { 0 };
+  // ci.nextOffset =  { 0 };
+  // ci.code = { 0 };
+  // ci.codeLen = { 0 };
+  // ci.features = { 0 };
 
   distorm_decompose(&ci, result, offset, &instructions_count);
   uint64_t ptr_size = 0;
