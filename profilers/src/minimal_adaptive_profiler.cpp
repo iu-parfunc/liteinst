ecd0581278bd4510603c664a6ef013ac4d739dbe
Joel Svensson <svenssonjoel@yahoo.se>
2015-07-28 14:10:34 +0200

    fixing forgotten leftover

--- profilers/src/minimal_adaptive_profiler.cpp
+++ profilers/src/minimal_adaptive_profiler.cpp
@@ -105,7 +105,7 @@ TLStatistics* minimalAdaptiveEpilogFunction(uint16_t func_id) {
   local_func_stats->total_time += elapsed;
   // There is a chance this condition might be satistified during the calibrations at init time
   // Skip deactivating if that's the case
-  if (new_count >= (global_func_stats->sample_size/thread_count) && g_ubiprof_initialized) {  
+  if (new_count >= global_func_stats->sample_size && g_ubiprof_initialized) {  
     if (__sync_bool_compare_and_swap(&(global_func_stats->lock), 0 , 1)) {
       if (PROFILER_INSTANCE->deactivateFunction(func_id) != -1) {
         global_func_stats->deactivation_count++; // Store in thread local structure and we sum all TL stuff when flushing results
