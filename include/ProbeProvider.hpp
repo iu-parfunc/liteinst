20178b2b836503e4217b59e3147cb5f30851f8a6
Ryan Newton <rrnewton@gmail.com>
2015-08-12 09:38:46 -0400

    Add initialize() step to the ProbeProvider

--- include/ProbeProvider.hpp
+++ include/ProbeProvider.hpp
@@ -18,10 +18,10 @@ enum ProbeContext { ENTRY, EXIT, LINE_NUM };
 /// and to null otherwise.
 typedef uint64_t ProbeLoc;
 
-/// Probes are a three-state finite automata, and all transitions
+/// Probes are a four-state finite automata, and all transitions
 /// between states must be atomic (i.e. have serialization points in
 /// the code).
-enum ProbeState { ACTIVE, INACTIVE, INITIALIZING };
+enum ProbeState { UNINITIALIZED, INITIALIZING, ACTIVE, DEACTIVATED };
 
 /// An opaque, unique probe identifier.  Do not depend on the
 /// representation of this value.
@@ -46,12 +46,24 @@ typedef struct ProbeMetaData {
 /// The signature for a callback that registers a newly discovered
 /// probe.  The ProbeProvider owns the ProbeMetadata record, so the
 /// callback may read it, but should not free it.
+///
+/// This probe-discovery callback has two main obligations:
+///
+///  (1) Call initialize() to set up the constant argument to future
+///      probe invocations.
+///
+///  (2) Call either activate() or deactivate() any number of times,
+///      to leave the probe in a valid state by callback completion.
+///
 typedef void (*Callback) (const ProbeMetaData* pmd);
 
 /// This is the type of function pointers for dynamically injected
 /// function calls.  The job of the ProbeProvider
 typedef void (*Instrumentation_func) (FuncId func_id);
 
+/// The argument passed at runtime to the Instrumentation_func.
+/// Very often, this is the FuncId.
+typedef uint64_t ProbeArg;
 
 /// Implements an object which discovers probes and subsequently
 /// provides the ability to toggle those probes.
@@ -67,6 +79,25 @@ class ProbeProvider {
      */
     ProbeProvider(Callback callback);
 
+
+    /// Move from UNINITIALIZED to INITIALIZING state.
+    /* Begin initializing the probe, moving from UNINITIALIZED to
+     * INITIALIZING state.  This function should not be called
+     * concurrently, it should only be called on a single thread, and
+     * specifically should be called from the probe discovery
+     * Callback.
+     *
+     * This function cannot fail, but it may raise an exception if
+     * preconditions are violated either in terms of the wrong
+     * starting state or detection of contention.
+     *
+     * \param probe_arg A constant argument that is passed to EVERY
+     * subsequent probe invocation, that is, every
+     * Instrumentation_func which the probe is activated with.
+     *
+     */
+    virtual void initialize(ProbeArg probe_arg);
+
     /// Activates the given probe.
     /* Activates the probe with given instrumentation function.
      *
@@ -81,13 +112,23 @@ class ProbeProvider {
      * with a different instrumentation function.  This may be relaxed
      * in the future.
      *
+     * It is an error to call this function before initialize
+     * (UNINITIALIZED state).  INITIALIZING or DEACTIVATED states are
+     * fine.
+     *
      * \param probe_id The probe id opaque identifier
      * \param func The instrumentation function
      */
     virtual bool activate(ProbeId probe_id, Instrumentation_func func) = 0;
 
     /// Deactivates the given probe
-    /* \param  The probe id: an opaque identifier.
+    /* Deactivate the probe, restoring the original functioality of
+     * the machine code at the probe point.
+     *
+     * It is an error to call this function before initialize
+     * (UNINITIALIZED state).  INITIALIZING or ACTIVE states are fine.
+     *
+     * \param  The probe id: an opaque identifier.
      *
      * \return A boolean indicating success or failure.  True implies
      * that the probe was successfully deactivated.
