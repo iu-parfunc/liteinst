27adbbb419d4e3bd92ef502a8825256a414a3222
Ryan Newton <rrnewton@gmail.com>
2015-08-18 00:08:22 -0400

    Minor: switch the order of jenkins test steps

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -5,11 +5,11 @@ set -e
 
 make clean
 
-# build run-benchmarks.exe via stack:
-make -f Make_runbench run-benchmarks.exe
-
 # Build the docker image:
 make docker
 
 # Run the tests inside the docker image:
 make testdocker
+
+# build run-benchmarks.exe via stack:
+make -f Make_runbench run-benchmarks.exe
