e79d9442c8e56539ba95eeef81e5a897cdd010a1
budkahaw <budkahaw@indiana.edu>
2015-11-18 23:06:43 -0500

    Adding Dtrace sample

---
+++ microbenchmarks/dtrace/looper.c
@@ -0,0 +1,16 @@
+#include <strings.h>
+
+int func(char* input) {
+  return (strlen(input)+1);
+}
+
+int main() {
+  int i,j,sum = 0;
+  for (i=0; i<1000000; i++) {
+    for (j=0; j<100; j++) {
+      sum +=func("hello Dtrace");
+    }
+  }
+
+  return (0);
+}
