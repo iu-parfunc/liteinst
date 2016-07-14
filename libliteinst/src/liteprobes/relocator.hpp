1895b16ce1c2ec911ef9a947dafcf3545dde37a8
chamibuddhika <chamibuddhika@gmail.com>
2016-07-14 01:26:46 -0400

    Comments on relocator API

--- libliteinst/src/liteprobes/relocator.hpp
+++ libliteinst/src/liteprobes/relocator.hpp
@@ -9,12 +9,21 @@ namespace liteprobes {
 
 class Relocations {
   public:
-    int n_instructions;
-    int* relocation_offsets;
+    int n_instructions; ///< Number of instructions relocated
+    int* relocation_offsets; ///< Offsets of the start address of each 
+                             ///< relocated instruction from the start of the
+                             ///< relocation buffer
 };
   
 class Relocator {
   public:
+
+    /** /brief Relocates the region of code from start to end (exclusive) to 
+     *    target address      
+     *  /param start Start address of the region
+     *  /param end   End address of the region (exclusive)
+     *  /return      Relocation information 
+     */
     Relocations relocate(utils::Address start, utils::Address end,
         utils::Address target);
 
