67ee0c09a3002952a7e3a773d7d32760a901cb45
budkahaw <budkahaw@indiana.edu>
2016-08-03 14:19:23 -0400

    Added signal handler

--- libliteinst/tests/apps/test_sig.cpp
+++ libliteinst/tests/apps/test_sig.cpp
@@ -1,8 +1,39 @@
 
+#include <stdio.h>
+#include <signal.h>
+#include <stdint.h>
+#include <string.h>
+
 extern "C" void dummy();
+
+typedef uint8_t* Address;
+
+void noop(int signum, siginfo_t* siginfo, void* context) {
+  ucontext_t* ucontext = (ucontext_t*) context;
+  Address interrupted_addr = (Address)(ucontext->uc_mcontext.gregs[REG_RIP]);
+  printf("Interrupted address : %p\n", interrupted_addr);
+  fflush(stdout);
+  // assert(false);
+  ucontext->uc_mcontext.gregs[REG_RIP] = (greg_t)interrupted_addr+1;
+}
+
+void registerHandler() {
+  struct sigaction act;
+  memset( &act, 0, sizeof act);
+  act.sa_flags = SA_SIGINFO; 
+  act.sa_sigaction = &noop;
+
+  sigemptyset(& (act.sa_mask));
+  sigaction(SIGILL, &act, NULL);
+
+  printf("Registered the SIGILL handler..\n");
+}
 
 int main() {
   // dummy();
+
+  registerHandler();
+
   while(true) {
     __asm(".byte 0x62");
     // __asm(".byte 0x62");
