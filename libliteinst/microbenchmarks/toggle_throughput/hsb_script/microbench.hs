beb63900102c481f8acaf243e5d8754e6c651b91
Joel Svensson <svenssonjoel@yahoo.se>
2016-08-11 21:19:08 +0200

    benchharness tweaks

--- libliteinst/microbenchmarks/toggle_throughput/hsb_script/microbench.hs
+++ libliteinst/microbenchmarks/toggle_throughput/hsb_script/microbench.hs
@@ -19,26 +19,26 @@ import GHC.Conc (getNumProcessors)
 
 main :: IO ()
 main = do
-  putStrLn "PATCH TOGGLE THROUGHPUT  MICROBENCH"
+  putStrLn "liteinst: toggle_throughput"
 
   defaultMainModifyConfig $ \ conf ->
     conf{ benchlist = benches
         , runTimeOut = Just 600
         , plugIns = [-- SomePlugin defaultFusionPlugin,
                      SomePlugin defaultDribblePlugin]
-        , harvesters = customTagHarvesterInt    "NUM_THREADS"  `mappend`
+        , harvesters = customTagHarvesterInt    "NUMBER_OF_EXECUTERS"  `mappend`
                        customTagHarvesterDouble "TARGET_TIME" `mappend`
                        customTagHarvesterDouble "ELAPSED_TIME" `mappend`
-                       customTagHarvesterDouble "MINIMUM_SWITCHES" `mappend`
-                       customTagHarvesterDouble "MAXIMUM_SWITCHES" `mappend`
+                      -- customTagHarvesterDouble "MINIMUM_SWITCHES" `mappend`
+                      -- customTagHarvesterDouble "MAXIMUM_SWITCHES" `mappend`
                        customTagHarvesterDouble "NUMBER_OF_TOGGLES" `mappend`
-                       customTagHarvesterInt    "NUMBER_OF_EXECUTERS" `mappend`
+                      -- customTagHarvesterInt    "NUMBER_OF_EXECUTERS" `mappend`
                        customTagHarvesterInt    "STRADDLE_POINT" `mappend`
-                       customTagHarvesterDouble "OBSERVED_SWITCHES_TOTAL" `mappend`
+                       --customTagHarvesterDouble "OBSERVED_SWITCHES_TOTAL" `mappend`
                        customTagHarvesterDouble "MINIMUM_FOO_CALLS" `mappend`
                        customTagHarvesterDouble "MAXIMUM_FOO_CALLS" `mappend`
-                       customTagHarvesterDouble "MINIMUM_BAR_CALLS" `mappend`
-                       customTagHarvesterDouble "MAXIMUM_BAR_CALLS" `mappend`
+                       --customTagHarvesterDouble "MINIMUM_BAR_CALLS" `mappend`
+                       --customTagHarvesterDouble "MAXIMUM_BAR_CALLS" `mappend`
                        customTagHarvesterDouble "TOTAL_FOO_CALLS" `mappend`
                        customTagHarvesterDouble "TOTAL_BAR_CALLS" `mappend`
                        customTagHarvesterDouble "TOTAL_CALLS" `mappend`
@@ -52,11 +52,13 @@ benches =
   --    [show threads, show duration, show rate]
   --    (Set (Variant "fastinst") (RuntimeEnv "IGNORE_THIS" "0"))
   --    | threads  <- threadss, duration <- durations, rate <- rates]
-  [mkBenchmark ("onoff-throughput")
+  [mkBenchmark ("toggle_throughput")
    [show threads, show duration, show rate]
-   (Set (Variant "fastinst") (CompileParam "-DONOFF_TOGGLING"))
+   (And [])
    | threads  <- threadss, duration <- durations, rate <- rates]
 
-threadss  = [0..15]
+
+-- (Set (Variant "liteinst") (CompileParam "-DONOFF_TOGGLING"))
+threadss  = [1..15]
 durations = [3.0]
 rates     = [0,10,100,1000,10000,100000,500000,1000000,10000000,100000000]
